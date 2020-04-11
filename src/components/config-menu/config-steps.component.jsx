9429bd3f9911b9b83b86ce8bd08533a042acd647
chace <christianlarcomb@gmail.com>
2020-04-10 21:52:19 -0400

    Minor-big update
    Semi-functioning overlay action with the 'Step 1' of the configuration process! Still working on completeing the configuration process then will step back to clean up code... Pushing this to master as v1.2

--- src/components/config-menu/config-steps.component.jsx
+++ src/components/config-menu/config-steps.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../../stylesheets/config-menu/config-steps.css"
+import "../../stylesheets/config-menu/config-overlay.css"
 
 class ConfigurationMenu extends React.Component
 {
@@ -8,9 +9,13 @@ class ConfigurationMenu extends React.Component
     {
         super(props);
 
+        this.overlayRef = React.createRef();
+
         this.state =
             {
-                step_one_completed: false
+                step_one_completed: false,
+                config_overlay_opened: false,
+                anim_triggered: false
             }
 
     }
@@ -20,28 +25,47 @@ class ConfigurationMenu extends React.Component
         /* If Step 1 is not completed yet... */
         if(!this.state.step_one_completed)
         {
+            /* Configure Button */
             return(
                 <div className="sel-menu-cont top">
                     <span className="txt-32px txt-bld">step 1</span>
-                    <button className="config-menu-button txt-18px">configure</button>
+
+                    <button className="config-menu-button txt-18px" onClick={this.handleOpening}>
+                        configure
+                    </button>
+
                 </div>
             )
 
         } else {
-
+            /* Successfully Completed Button! */
             return(
                 <div className="sel-menu-cont top">
                     <span className="txt-32px txt-bld txt-clr-success">step 1</span>
 
                     <button className="config-menu-button-success">
+
                         <svg className="checkmark-sml" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 515.556 515.556">
                             <path fill="white" d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z"/>
                         </svg>
+
                     </button>
 
                 </div>
             )
         }
+    };
+
+    handleOpening = () =>
+    {
+
+        let currentClassNameArray = this.overlayRef.current.className.split(" ");
+
+        this.overlayRef.current.className = currentClassNameArray[0] + " view-enabled";
+        setTimeout(() => {
+            this.overlayRef.current.className = this.overlayRef.current.className.toString() + " view-enabled-anim"
+        }, 10)
+
     };
 
     /* Renders the enabled or disabled drag and drop config-menu */
@@ -100,19 +124,26 @@ class ConfigurationMenu extends React.Component
       }
     };
 
-
     render()
     {
         return(
-            <div className="sel-menu-wrapper">
+            <>
 
-                { this.stepOneRender() }
+                <div ref={this.overlayRef} className="config-overlay-container view-disabled">
 
-                <br/>
+                </div>
 
-                { this.stepTwoRender() }
 
-            </div>
+                <div className="sel-menu-wrapper">
+
+                    { this.stepOneRender() }
+
+                    <br/>
+
+                    { this.stepTwoRender() }
+
+                </div>
+            </>
         )
     }
 
